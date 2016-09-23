04a96a625a4d124d92802c35dc1b326217921e15
Alex Banks <alex@moonhighway.com>
2016-09-22 17:24:44 -0700

    names functions in fetch and disptach sample

--- chapter-12/color-organizer/src/actions.js
+++ chapter-12/color-organizer/src/actions.js
@@ -1,10 +1,14 @@
 import fetch from 'isomorphic-fetch'
 
+const parseResponse = response => response.json()
+
+const logError = error => console.error(error)
+
 const fetchThenDispatch = (dispatch, url, method, body) =>
     fetch(url, {method, body, headers: { 'Content-Type': 'application/json' }})
-        .then(response => response.json())
-        .then(action => dispatch(action))
-        .catch(error => console.error(error))
+        .then(parseResponse)
+        .then(dispatch)
+        .catch(logError)
 
 export const addColor = (title, color) => dispatch =>
     fetchThenDispatch(
