24d754db1371365aee1dba2c12b6015ad6a698fe
MoonTahoe <alex@moonhighway.com>
2017-03-20 11:39:28 -0700

    Working Fix - populate

--- chapter-03/populate-united-states.js
+++ chapter-03/populate-united-states.js
@@ -1,7 +1,7 @@
 /*  Promises
  **********************************************************************/
 const getFakeMembers = count => new Promise((resolves, rejects) => {
-    const api = `http://api.randomuser.me/?nat=US&results=${count}`
+    const api = `https://api.randomuser.me/?nat=US&results=${count}`
     const request = new XMLHttpRequest()
     request.open('GET', api)
     request.onload = () => (request.status == 200) ? resolves(JSON.parse(request.response).results) : reject(Error(request.statusText))
