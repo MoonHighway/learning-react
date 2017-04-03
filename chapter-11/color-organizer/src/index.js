878dea653c0f22bfc37b2cf7b9316816595374a2
MoonTahoe <alex@moonhighway.com>
2017-04-03 15:23:26 -0700

    Moved All router code into App.js

--- chapter-11/color-organizer/src/index.js
+++ chapter-11/color-organizer/src/index.js
@@ -1,6 +1,5 @@
 import React from 'react'
 import { render } from 'react-dom'
-import { HashRouter, Route, NavLink } from 'react-router-dom'
 import { Provider } from 'react-redux'
 import App from './components/App'
 import storeFactory from './store'
@@ -10,14 +9,9 @@ const store = storeFactory()
 window.React = React
 window.store = store
 
-// TODO: Setup Whoops404 Routes
-// TODO: Change Readme, push all updates
-
 render(
     <Provider store={store}>
-       <HashRouter>
-         <App />
-       </HashRouter>
+        <App />
     </Provider>,
     document.getElementById('react-container')
 )
