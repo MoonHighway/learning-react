8630eedcb3934235a2e68be69913de416fdef051
MoonTahoe <alex@moonhighway.com>
2017-09-27 14:06:25 -0700

    React 16 Updates for Chapter 6

--- chapter-06/color-organizer/src/components/AddColorForm.js
+++ chapter-06/color-organizer/src/components/AddColorForm.js
@@ -1,4 +1,5 @@
-import { PropTypes, Component } from 'react'
+import { Component } from 'react'
+import PropTypes from 'prop-types'
 import '../../stylesheets/AddColorForm.scss'
 
 const AddColorForm = ({onNewColor=f=>f}) => {
