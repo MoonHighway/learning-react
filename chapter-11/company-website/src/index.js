e020939943a523d55a8780c308ac0d7cdb4f11a3
Aarif Khamdi <aarifkhamdi@gmail.com>
2019-05-08 02:41:41 +0500

    redirecting fix (#38)

--- chapter-11/company-website/src/index.js
+++ chapter-11/company-website/src/index.js
@@ -24,10 +24,10 @@ render(
     <div className="main">
       <Switch>
         <Route exact path="/" component={Home} />
-        <Route path="/about" component={About} />
         <Redirect from="/history" to="/about/history" />
         <Redirect from="/services" to="/about/services" />
         <Redirect from="/location" to="/about/location" />
+        <Route path="/about" component={About} />
         <Route path="/events" component={Events} />
         <Route path="/products" component={Products} />
         <Route path="/contact" component={Contact} />
